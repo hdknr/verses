348d60cafc9a002d4444e4ea1a078f848540a380
user.name <gmail@hdknr.com>
2022-03-30 19:55:17 +0900

    try to ssh if one instance was found

--- verses/aws/ec2/commands.py
+++ verses/aws/ec2/commands.py
@@ -13,13 +13,13 @@ def ec2(ctx):
 @click.argument("tags", nargs=-1)
 @click.pass_context
 def ssh(ctx, tags):
-    print(ctx.obj["env"])
     filters = [
         {"Name": f"tag:{k}", "Values": [v]} for k, v in [i.split("=") for i in tags]
     ]
     res = instance.query(None, filters=filters)
-    if len(res.Reservations):
+    if len(res.Reservations) == 1:
         print(res.Reservations[0].Instances[0].PublicIpAddress)
         print(res.Reservations[0].Instances[0].KeyName)
         return
-    breakpoint()
+    # TODO:
+    # list of instances
