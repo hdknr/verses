e70c384a4357c47e9a57f35b9918db5caa0dea21
user.name <gmail@hdknr.com>
2022-04-10 19:02:53 +0900

    aws ec2 instances subcommands

--- verses/aws/ec2/commands.py
+++ verses/aws/ec2/commands.py
@@ -1,12 +1,7 @@
-from datetime import datetime
-
 import click
 
-from verses.aws.base import call
-from verses.base import logs, remote
 
 from . import instances
-from .base import client
 
 
 @click.group()
@@ -15,68 +10,4 @@ def ec2(ctx):
     pass
 
 
-@ec2.command()
-@click.argument("name")
-@click.option("--user", "-u", default="ubuntu")
-@click.pass_context
-def ssh_conf(ctx, name, user):
-    """create ssh config file to EC2 instance"""
-    instance = instances.query_by_name(None, name)
-    # https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ec2.html#EC2.Client.describe_instances
-    if not instance:
-        logs.message(dict(error=f"no instance of {name}"))
-        return
-
-    keyname, ipaddress = instance.KeyName, instance.PublicIpAddress
-
-    key = remote.ssh_keyfile(f"{keyname}.cer", f"{keyname}.pem")
-    if not key:
-        logs.message(dict(error=f"{keyname} was not found."))
-        return
-
-    remote.update_ssh_conf(
-        name,
-        user,
-        "server",
-        ipaddress,
-        str(key),
-    )
-
-
-@ec2.command()
-@click.argument("name")
-@click.argument("tags", metavar="{key=value}", nargs=-1)
-@click.option("--suffix", "-s", default=None)
-@click.option("--reboot", "-r", is_flag=True)
-@click.option("--dry_run", "-d", is_flag=True)
-@click.pass_context
-def create_image(ctx, name, tags, suffix, reboot, dry_run):
-    """create ami for named instance
-
-    sample:
-    create-image
-        prod-masters myservice-deploy=prod myservice-server=masters
-            serial=$(date +"%Y%m%d%H%M%S") --suffix $(date +"%Y%m%d%H%M%S")
-    """
-    instance = instances.query_by_name(None, name)
-    if not instance:
-        logs.message(dict(error=f"no instance of {name}"))
-        return
-
-    now = datetime.now()
-    suffix = suffix or now.strftime("%Y%m%d")
-
-    instance_id = instance.InstanceId
-    name = f"{name}-{suffix}"
-    keyvalues = dict(tuple(t.split("=")) for t in tags)
-    tag_specs = instances.tag_specifications("image", keyvalues)
-
-    res = call(
-        client().create_image,
-        InstanceId=instance_id,
-        Name=name,
-        TagSpecifications=tag_specs,
-        NoReboot=not reboot,
-        DryRun=dry_run,
-    )
-    logs.message(res)
+ec2.add_command(instances.instances)
--- verses/aws/ec2/instances.py
+++ verses/aws/ec2/instances.py
@@ -1,4 +1,9 @@
-from verses.aws.base import Object
+from datetime import datetime
+
+import click
+
+from verses.aws.base import Object, call
+from verses.base import logs, remote
 
 from ..base import filters
 from .base import client
@@ -25,3 +30,76 @@ def tag_specifications(res_type, keyvalues):
             "Tags": [{"Key": key, "Value": value} for key, value in keyvalues.items()],
         }
     ]
+
+
+@click.group()
+@click.pass_context
+def instances(ctx):
+    pass
+
+
+@instances.command()
+@click.argument("name")
+@click.option("--user", "-u", default="ubuntu")
+@click.pass_context
+def ssh_conf(ctx, name, user):
+    """create ssh config file to EC2 instance"""
+    instance = query_by_name(None, name)
+    # https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ec2.html#EC2.Client.describe_instances
+    if not instance:
+        logs.message(dict(error=f"no instance of {name}"))
+        return
+
+    keyname, ipaddress = instance.KeyName, instance.PublicIpAddress
+
+    key = remote.ssh_keyfile(f"{keyname}.cer", f"{keyname}.pem")
+    if not key:
+        logs.message(dict(error=f"{keyname} was not found."))
+        return
+
+    remote.update_ssh_conf(
+        name,
+        user,
+        "server",
+        ipaddress,
+        str(key),
+    )
+
+
+@instances.command()
+@click.argument("name")
+@click.argument("tags", metavar="{key=value}", nargs=-1)
+@click.option("--suffix", "-s", default=None)
+@click.option("--reboot", "-r", is_flag=True)
+@click.option("--dry_run", "-d", is_flag=True)
+@click.pass_context
+def create_image(ctx, name, tags, suffix, reboot, dry_run):
+    """create ami for named instance
+
+    sample:
+    create-image
+        prod-masters myservice-deploy=prod myservice-server=masters
+            serial=$(date +"%Y%m%d%H%M%S") --suffix $(date +"%Y%m%d%H%M%S")
+    """
+    instance = query_by_name(None, name)
+    if not instance:
+        logs.message(dict(error=f"no instance of {name}"))
+        return
+
+    now = datetime.now()
+    suffix = suffix or now.strftime("%Y%m%d")
+
+    instance_id = instance.InstanceId
+    name = f"{name}-{suffix}"
+    keyvalues = dict(tuple(t.split("=")) for t in tags)
+    tag_specs = instances.tag_specifications("image", keyvalues)
+
+    res = call(
+        client().create_image,
+        InstanceId=instance_id,
+        Name=name,
+        TagSpecifications=tag_specs,
+        NoReboot=not reboot,
+        DryRun=dry_run,
+    )
+    logs.message(res)
